1330e2b0c9f8a14d1470ba3fcc2936c6070c0da0
ConaGo <l_jasper@web.de>
2021-06-08 10:23:50 +0200

    cleanup data_parser; added tag to remove gorm-model entries when parsing to json

--- data_parser/data_parser.go
+++ data_parser/data_parser.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"reflect"
 	"strings"
 	"time"
 
@@ -89,7 +88,7 @@ func ParseData() {
 	}
 	db.AutoMigrate(&Conference{})
 	db.AutoMigrate((&Tag{}))
-	datas := readConferenceData2()
+	datas := readConferenceData()
 	db.Create(&datas)
 	var conferences []Conference
 	for _, elem := range conferences {
@@ -97,126 +96,8 @@ func ParseData() {
 	}
 }
 
-// Converts parsed json from "map[string]interface" format to the "Conference" struct
-// Dates get converted to time.Time
-// Tags get converted from string to array of "Tag"-structs
-// html_parser gets called to populate HTMLMetadata
-// func convertConferenceData(d []map[string]interface{}) []Conference {
-// 	var confs []Conference
-// 	const (
-// 		layoutISO = "2006-01-02"
-// 	)
-// 	for _, data := range d {
-// 		c := Conference{
-// 			Name:      "",
-// 			Url:       "",
-// 			StartDate: time.Now(),
-// 			EndDate:   time.Now(),
-// 			City:      "",
-// 			Country:   "",
-// 			Online:    false,
-// 			Twitter:   "",
-// 			Tags:      []Tag{},
-// 		}
-// 		for key := range data {
-// 			switch key {
-// 			case "name":
-// 				c.Name = reflect.ValueOf(data["name"]).String()
-// 			case "url":
-// 				c.Url = reflect.ValueOf(data["url"]).String()
-// 			case "startDate":
-// 				str := reflect.ValueOf(data["startDate"]).String()
-// 				c.StartDate, _ = time.Parse(layoutISO, str)
-// 			case "endDate":
-// 				str := reflect.ValueOf(data["endDate"]).String()
-// 				c.EndDate, _ = time.Parse(layoutISO, str)
-// 			case "city":
-// 				c.City = reflect.ValueOf(data["city"]).String()
-// 			case "country":
-// 				c.Country = reflect.ValueOf(data["country"]).String()
-// 			case "online":
-// 				if reflect.ValueOf(data["online"]).Bool() {
-// 					c.Online = true
-// 				} else {
-// 					c.Online = false
-// 				}
-// 			case "twitter":
-// 				c.Twitter = reflect.ValueOf(data["twitter"]).String()
-// 			case "tags":
-// 				for _, el := range strings.Fields(reflect.ValueOf(data["tags"]).String()) {
-// 					c.Tags = append(c.Tags, Tag{TagName: el})
-// 				}
-// 			}
 
-// 		}
-// 		c.Metadata = html_parser.GetHTMLMeta(c.Url)
-// 		confs = append(confs, c)
-// 	}
-// 	return confs
-// }
-// func readConferenceData() []map[string]interface{} {
-// 	var rootPath string = "data/conference-data/conferences"
-// 	var parsedJson []map[string]interface{}
-// 	var currentDir string
-// 	filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-// 		if err != nil {
-// 			log.Fatalf(err.Error())
-// 		}
-// 		if info.IsDir() {
-// 			currentDir = info.Name()
-// 			fmt.Printf("directory name: %s\n", currentDir)
-// 		} else if currentDir == "2022" {
-// 			path := rootPath + "/" + currentDir + "/" + info.Name()
-// 			fmt.Println(path)
-// 			jsonFile, e := os.Open(path)
-// 			if e != nil {
-// 				fmt.Println(e)
-// 			}
-// 			// defer the closing of our jsonFile so that we can parse it later on
-// 			defer jsonFile.Close()
-// 			// read our opened xmlFile as a byte array.
-// 			byteJson, _ := ioutil.ReadAll(jsonFile)
-
-// 			var newParsedJson []map[string]interface{}
-// 			json.Unmarshal([]byte(byteJson), &newParsedJson)
-
-// 			// adding the filename as a tag : because the raw data encodes the tags through the filename
-// 			for _, re := range newParsedJson {
-// 				// remove .json file ending
-// 				arr := strings.Split(info.Name(), ".")[0]
-// 				// add new "tags" field to the elements
-// 				re["tags"] = arr
-// 			}
-// 			parsedJson = append(parsedJson, newParsedJson...)
-// 		}
-// 		return nil
-// 	})
-
-// 	// The data contains duplicate entries
-// 	// (because tags are stored in the filenames in the original data),
-// 	// so we remove those while collecting the "tags"
-// 	var parsedJsonAsSet []map[string]interface{}
-// 	for i, val1 := range parsedJson {
-// 		shouldAdd := true
-// 		fmt.Println(reflect.ValueOf(val1["name"]).String())
-// 		for j, val2 := range parsedJson {
-// 			if j > i && compare(val1, val2) {
-// 				val2["tags"] = reflect.ValueOf(val2["tags"]).String() + " " + reflect.ValueOf(val1["tags"]).String()
-// 				fmt.Println(reflect.ValueOf(val2["tags"]).String())
-// 				shouldAdd = false
-// 			}
-// 		}
-// 		if shouldAdd {
-// 			parsedJsonAsSet = append(parsedJsonAsSet, val1)
-// 		}
-// 	}
-// 	for _, val := range parsedJsonAsSet {
-// 		fmt.Println(reflect.ValueOf(val["tags"]).String())
-// 	}
-// 	return parsedJsonAsSet
-// }
-
-func readConferenceData2() []*Conference {
+func readConferenceData() []*Conference {
 	rootPath := "data/conference-data/conferences"
 	var allConferences []*Conference
 	var currentDir string
@@ -267,7 +148,7 @@ func readConferenceData2() []*Conference {
 	for i, conference := range allConferences {
 		shouldAdd := true
 		for j, c := range allConferences {
-			if j > i && compare2(conference, c) {
+			if j > i && compare(conference, c) {
 				c.Tags = append(c.Tags, conference.Tags...)
 				shouldAdd = false
 			}
@@ -280,12 +161,8 @@ func readConferenceData2() []*Conference {
 	}
 	return filteredConferences
 }
-func compare2(a, b *Conference) bool {
+// the compare function returns true if "name" and "startDate" field is equal
+func compare(a, b *Conference) bool {
 	// log.Println("found duplicate", a.Name == b.Name && a.StartDate == b.StartDate)
 	return a.Name == b.Name && a.StartDate == b.StartDate
-}
-
-// the compare function returns true if "name" and "startDate" field is equal
-func compare(val1, val2 map[string]interface{}) bool {
-	return reflect.ValueOf(val1["name"]).String() == reflect.ValueOf(val2["name"]).String() && reflect.ValueOf(val1["startDate"]).String() == reflect.ValueOf(val2["startDate"]).String()
 }
