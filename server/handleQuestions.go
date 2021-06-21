1c76ad317b8423664ae85f12ceb22ff6b897d8eb
ConaGo <l_jasper@web.de>
2021-06-21 05:24:56 +0200

    question token advanced

--- server/handleQuestions.go
+++ server/handleQuestions.go
@@ -5,10 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 	"time"
-
-	"database/sql/driver"
 
 	"conago.de/web-scraper/db/mongo"
 	"conago.de/web-scraper/db/sqlite"
@@ -30,7 +27,7 @@ func HandleQuestions(w http.ResponseWriter, r *http.Request) {
 	amount := params.Get("amount")
 	fmt.Println(amount)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-defer cancel()
+	defer cancel()
 	mongo.QuizDB.DB.Find(ctx, bson.D{})
 }
 type Token struct {
@@ -40,70 +37,26 @@ type Token struct {
 }
 type Question struct {
 	gorm.Model		`json:"-"`
-	MongoI string
-}
-type mongoID primitive.ObjectID
-func (id *mongoID) Scan(src interface{}) error {
-	if t, ok := src.(primitive.ObjectID); ok {
-		*id = mongoID(t)
-	}
-	return nil
-}
-func (id mongoID) Value() (driver.Value, error) {
-	return fmt.Sprint(id), nil
+	MongoID string
 }
 func Test() {
-	//sqlite.TokenDB.DB.AutoMigrate((&Token{}))
-
+	sqlite.TokenDB.DB.AutoMigrate((&Token{}))
 	sqlite.TokenDB.DB.AutoMigrate((&Question{}))
-	//var q = []question{{MongoI: "jklsad"}, {MongoI: "hjk"}}
-	//sqlite.TokenDB.DB.Create(&q)
+
+	
 	var ctx = context.TODO()
 
-	//FindALL
-/* 	cursor, err := mongo.QuizDB.DB.Find(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-		return 
-	}
-	var questions []bson.M 
-	if err = cursor.All(ctx, &questions); err != nil {
-		log.Fatal(err)
-	} 
-		fmt.Println((questions))
-	*/
 	var question bson.M
 	if err := mongo.QuizDB.DB.FindOne(ctx, bson.M{}).Decode(&question); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(question["_id"])
-	fmt.Println("_________")
-	//idq := question["_id"].(primitive.ObjectID)
-	xType := reflect.TypeOf(fmt.Sprint(question["_id"]))
-xValue := reflect.ValueOf(fmt.Sprint(question["_id"]))
-
-fmt.Println(xType, xValue) 
-
-	fmt.Println(fmt.Sprint(question["_id"]))
-	q := Question{MongoI: fmt.Sprint(question["_id"])}
-	fmt.Println(q.MongoI)
+	mongoID := question["_id"].(primitive.ObjectID).Hex()
+	q := Question{MongoID: mongoID}
 	sqlite.TokenDB.DB.Create(&q)
 
 	questions := []Question{}
 	sqlite.TokenDB.DB.Find(&questions)
 	fmt.Println(questions)
-
-
-
-
-/* 	db.AutoMigrate(&Conference{})
-	db.AutoMigrate((&Tag{}))
-	datas := readConferenceData()
-	db.Create(&datas)
-	var conferences []Conference
-	for _, elem := range conferences {
-		fmt.Println(elem.Name, elem.City)
-	} */
 }
 
 
